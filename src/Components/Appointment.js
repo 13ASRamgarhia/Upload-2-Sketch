d8230bfb158f2a0d8ffda536ff0a377adcfd12f4
Amandeep Singh <13asramgarhia@gmail.com>
2024-04-03 07:11:34 -0400

    first commit

--- src/Components/Appointment.js
+++ src/Components/Appointment.js
@@ -1,40 +1,8 @@
 import React, { useState } from 'react'
-import { useRef } from 'react'
-import { Header,  Modal } from 'semantic-ui-react'
+import { Modal } from 'semantic-ui-react'
 
 function Appointment() {
   const [open, setOpen] = useState(false)
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    mobile: "",
-    companyName: "",
-    message: ""
-  })
-
-  const [formLoading, setFormLoading] = useState(false)
-  const formRef = useRef()
-  const [success, setSuccess] = useState("")
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormLoading(true)
-    console.log(formData)
-    setTimeout(() => {setFormLoading(false)}, 2000)
-    setTimeout(() => setSuccess("**This functionality is yet to be implemented**"), 2100)
-    setTimeout(() => {setOpen(false)}, 6000)
-    setTimeout(() => setSuccess(""), 6100)
-  }
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }
 
   return (
     <Modal
